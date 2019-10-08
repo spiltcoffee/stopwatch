0a00c6ad3d7266c23569ff2f07f4c7eb15dc6a74
spiltcoffee <spiltcoffee@users.noreply.github.com>
2019-10-08 21:42:43 +1100

    wip

--- src/main.js
+++ src/main.js
@@ -10,6 +10,23 @@ const bounds = require("./utils/bounds");
 const icon = path.resolve(__dirname, require("./favicon.ico"));
 const APP_ID = "SpiltCoffee.VRStopwatch";
 
+const WM_KEYDOWN = 0x0100; // https://docs.microsoft.com/en-au/windows/win32/inputdev/wm-keydown
+const WM_KEYUP = 0x0101; // https://docs.microsoft.com/en-au/windows/win32/inputdev/wm-keyup
+const VK_F1 = 0x70; // https://docs.microsoft.com/en-au/windows/win32/inputdev/virtual-key-codes
+
+const win32 = () => {
+  if (process.platform !== "win32") {
+    return null;
+  }
+
+  const ffi = require("ffi");
+
+  return new ffi.Library("user32", {
+    FindWindowA: ["long", ["string", "string"]], // https://docs.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-findwindowa
+    PostMessageA: ["int32", ["long", "int32", "long", "int32"]] // https://docs.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-postmessagea
+  });
+};
+
 let mainWindow;
 
 if (squirrel || !app.requestSingleInstanceLock()) {
@@ -109,4 +126,14 @@ ipcMain.on("finished", (_, session) => {
     mainWindow.flashFrame(true);
   }
   logger().session(session);
+});
+
+ipcMain.on("knock", () => {
+  if (win32) {
+    const handle = win32.FindWindowA(null, "Want my attention?");
+    if (handle) {
+      win32.PostMessageA(handle, WM_KEYDOWN, VK_F1, 0);
+      win32.PostMessageA(handle, WM_KEYUP, VK_F1, 0);
+    }
+  }
 });
